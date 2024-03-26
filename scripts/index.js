70ba5f360159692365b8cddbdc5ebfac9ee5fd8f
Sergey S. <karpseledka@gmail.com>
2024-03-27 01:17:43 +0700

    try #1.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+const cardList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+const cardElement = cardTemplate.querySelector('.card');
 
-// @todo: DOM узлы
+function createCard(imageLink, title, removeCard){
+  const cardElementClone = cardElement.cloneNode(true);
+  const cardImage = cardElementClone.querySelector('.card__image');
+  const cardTitle = cardElementClone.querySelector('.card__title');
+  const cardDeleteButton = cardElementClone.querySelector('.card__delete-button');
+  
+  cardImage.src = imageLink;
+  cardTitle.textContent = title;
+  cardDeleteButton.addEventListener('click', removeCard);
 
-// @todo: Функция создания карточки
+  return cardElementClone;
+}
 
-// @todo: Функция удаления карточки
+function removeCard(event) {
+  const cardElement = event.target.closest('.card')
+  cardElement.remove();
+}
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach(item => cardList.append(createCard(item.link, item.name, removeCard)));
